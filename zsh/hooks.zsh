d40f8d7e8be9a7c8117fc26ec2271e400590ba55
bkness <DevBrandon@icloud.com>
2026-05-02 08:34:36 -0700

    fixed a typo in venv deactivation function

--- zsh/hooks.zsh
+++ zsh/hooks.zsh
@@ -49,6 +49,14 @@ auto-venv() {
 }
 register_hook "on_dir_enter" "auto-venv"
 
+# Auto-close Python virtualenv when leaving directory
+_auto_deactivate_venv() {
+  if [[ -n "$VIRTUAL_ENV" && "$PWD" != "VIRTUAL_ENV"* ]]; then
+    deactivate
+  fi  
+}
+register_hook "on_dir_enter" "_auto_deactivate_venv"
+
 # Auto-switch Node version when .nvmrc is present
 auto-nvm() {
   [[ -f .nvmrc ]] || return
